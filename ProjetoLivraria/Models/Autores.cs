343b202efb4dde4834421be6e023a9290df001d6
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-07-10 07:59:08 -0300

    Adicionar Classe Autores

---
+++ ProjetoLivraria/Models/Autores.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoLivraria.Models
+{
+
+    [Serializable]
+    public class Autores
+    {
+        public decimal aut_id_autor { get; set; }
+        public string aut_nm_nome { get; set; }
+        public string aut_nm_sobrenome { get; set; }
+        public string aut_ds_email { get; set; }
+         
+        public Autores(decimal adcIdAutor, string asNomeAutor, string asSobrenomeAutor, string asEmailAutor)
+        {
+            this.aut_id_autor = adcIdAutor;
+            this.aut_nm_nome = asNomeAutor;
+            this.aut_nm_sobrenome = asSobrenomeAutor;
+            this.aut_ds_email = asEmailAutor;
+        }
+    }
+}
