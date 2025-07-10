5ac4928b735d099c322e17a3635fea3586881501
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-07-10 08:39:21 -0300

    Adicionar Classe TiposLivro

---
+++ ProjetoLivraria/Models/TiposLivro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoLivraria.Models
+{
+    [Serializable]
+    public class TiposLivro
+    {
+        public decimal til_id_tipo_livro { get; set; }
+        public string til_ds_descricao { get; set; }    
+
+        public TiposLivro(decimal adcIdTipoLivro, string asDescricao)
+        {
+            this.til_id_tipo_livro = adcIdTipoLivro;
+            this.til_ds_descricao = asDescricao;
+        }
+    }
+}
