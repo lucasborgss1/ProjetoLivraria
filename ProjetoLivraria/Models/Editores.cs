e3df9066480ee78e299b8f6106c9af7808945199
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-07-10 08:14:09 -0300

    Adicionar Classe Editores

---
+++ ProjetoLivraria/Models/Editores.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoLivraria.Models
+{
+    [Serializable]
+    public class Editores
+    {
+        public decimal edi_id_editor { get; set; }
+        public string edi_nm_editor { get; set; }
+        public string edi_ds_email { get; set; }
+        public string edi_ds_url { get; set; }
+
+        public Editores(decimal adcIdEditor, string asNomeEditor, string asEmailEditor, string asUrlEditor)
+        {
+            this.edi_id_editor = adcIdEditor;
+            this.edi_nm_editor = asNomeEditor;
+            this.edi_ds_email = asEmailEditor;
+            this.edi_ds_url = asUrlEditor;
+        }
+    }
+}
