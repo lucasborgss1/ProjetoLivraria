92a9f9c1c0d2258315f81f81846ec60bd7bb9168
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-07-10 08:37:48 -0300

    Atualizar Classe Livros

--- ProjetoLivraria/Models/Livros.cs
+++ ProjetoLivraria/Models/Livros.cs
@@ -17,5 +17,17 @@ namespace ProjetoLivraria.Models
         public string liv_ds_resumo { get; set; }
         public int liv_nu_edicao { get; set; }
 
+        public Livros(decimal adcIdLivro, decimal asIdTipoLivro, decimal asIdEditor, string asNomeTitulo, decimal asValorPreco,
+            decimal asRoyalty, string asDescricao, int asNumeroEdicao)
+        {
+            this.liv_id_livro = adcIdLivro;
+            this.liv_id_tipo_livro = asIdTipoLivro;
+            this.liv_id_editor = asIdEditor;
+            this.liv_nm_titulo = asNomeTitulo;
+            this.liv_vl_preco = asValorPreco;
+            this.liv_pc_royalty = asRoyalty;
+            this.liv_ds_resumo = asDescricao;
+            this.liv_nu_edicao = asNumeroEdicao;
+        }
     }
 }
