be0e403ca8b338d37d2e57f4e5b6383698914449
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-07-10 08:26:07 -0300

    Adicionar Classe Livros

---
+++ ProjetoLivraria/Models/Livros.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoLivraria.Models
+{
+    [Serializable]
+    public class Livros
+    {
+        public decimal liv_id_livro { get; set; }
+        public decimal liv_id_tipo_livro { get; set; }
+        public decimal liv_id_editor { get; set; }
+        public string liv_nm_titulo { get; set; }
+        public decimal liv_vl_preco { get; set; }
+        public decimal liv_pc_royalty { get; set; }
+        public string liv_ds_resumo { get; set; }
+        public int liv_nu_edicao { get; set; }
+
+    }
+}
